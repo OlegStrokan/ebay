a13d0d81a90817ac1337a2f6ff2714f7bb140ec9
Oleh Strokan <oleg14ua71@gmail.com>
2026-05-28 08:16:01 +0200

    gateway: update role permissions

--- Gateway/Gateway.Api/Endpoints/RoleEndpoints.cs
+++ Gateway/Gateway.Api/Endpoints/RoleEndpoints.cs
@@ -10,7 +10,7 @@ public static class RoleEndpoints
     {
         var group = routes.MapGroup("/api/v1/roles")
             .WithTags("Roles")
-            .RequireAuthorization();
+            .RequireAuthorization("AdminOnly");
 
         group.MapPost("/", async (CreateRoleRequest request, GrpcRole.RoleService.RoleServiceClient client) =>
         {
